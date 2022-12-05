c1be4903493a8179cbcaa6e23d97b2498c72db3e
koya171 <narendrakoya09@gmail>
2022-12-05 09:00:30 +0530

    My first commit node js tutorial

---
+++ 03.modules.js
@@ -0,0 +1,9 @@
+const names = require("./04.names");
+const sayHi = require("./05.utils");
+const data = require("./06-alternative-flavor");
+require("./07-mind-grenade");
+//console.log(data);
+//console.log(data.items);
+//console.log(data.singlePerson);
+sayHi(names.john);
+sayHi(names.peter);
---
+++ 08-os-module.js
@@ -0,0 +1,13 @@
+const os = require("os");
+
+console.log(os.userInfo());
+
+console.log(`The System Uptime is ${os.uptime()} seconds`);
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+};
+console.log(currentOS);
